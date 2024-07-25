02d161aea04eeb6071ca144417345b47e2d2d574
Ascencioncar <carlosrp1918@gmail.com>
2024-07-24 21:32:11 -0500

    Actividades semillero 24/07/24 segunda actividad realizada

---
+++ src/actividadessemillero/ActividadesSemillero.java
@@ -0,0 +1,20 @@
+package actividadessemillero;
+import java.util.Scanner;
+
+public class ActividadesSemillero {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("¿Cuantos numeros deseas ingresar para sumar y sacar promedio? ");
+    float cantidadNumeros = input.nextFloat();
+        
+        float suma = 0;
+        
+        for (int i = 0; i < cantidadNumeros; i++) {
+            System.out.print("Ingresa el número " + (i + 1) + ": ");
+            int numero = input.nextInt();
+            suma += numero/cantidadNumeros;
+        }
+        System.out.println("el promedio de los números ingresados es: " + suma);
+
+    }
+}
