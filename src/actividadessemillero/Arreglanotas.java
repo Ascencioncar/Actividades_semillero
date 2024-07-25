56a3146771e44a387fb4094ba15204a60125b290
Ascencioncar <carlosrp1918@gmail.com>
2024-07-24 21:35:37 -0500

    Actividades semillero 24/07/24 segunda actividad realizada

---
+++ src/actividadessemillero/Arreglanotas.java
@@ -0,0 +1,20 @@
+package actividadessemillero;
+import java.util.Scanner;
+
+public class Arreglanotas {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("¿Cuantas notas vas a ingresar? ");
+    float notas = input.nextFloat();
+        
+        float suma = 0;
+        
+        for (int i = 0; i < notas; i++) {
+            System.out.print("Ingresa el número " + (i + 1) + ": ");
+            float numero = input.nextFloat();
+            suma += numero/notas;
+        }
+        if (suma>=3.0){boolean nota =true; System.out.println("Las notas sumadas: " + suma + " Ganaste!" + nota);}
+        else{boolean nota =false; System.out.println("Perdiste geometría " + nota); }
+}
+}
