f94c26474b2a95a11f1500c39b9746fa7cfa7a25
Prodiagnostico <carlosrp1918@gmail.com>
2024-07-26 08:31:54 -0500

    Versión mejorada, cuarta actividad

--- src/actividadessemillero/Mayusculas.java
+++ src/actividadessemillero/Mayusculas.java
@@ -1,15 +1,25 @@
-
 package actividadessemillero;
 import java.util.Scanner;
 
 public class Mayusculas {
     public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.println("Digita una frase y este programa detectará si tiene mayuscula; ");
-    String texto = input.next();
-    input.close();
-    
-    texto.chars().forEach(l -> {
-    System.out.printf("%c is UpperCase %b\n", 1, Character.isUpperCase(l));});
-    
-}}
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digita una frase y este programa detectará si tiene mayúsculas:");
+        String texto = input.nextLine(); // Cambié next() a nextLine() para leer toda la frase
+        input.close();
+
+        boolean tieneMayuscula = false;
+        for (char c : texto.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                tieneMayuscula = true;
+                break;
+            }
+        }
+
+        if (tieneMayuscula) {
+            System.out.println("La frase contiene mayúsculas.");
+        } else {
+            System.out.println("La frase no contiene mayúsculas.");
+        }
+    }
+}
